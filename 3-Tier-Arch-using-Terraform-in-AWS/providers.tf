46031e22e5ea09bfc16d745a7062bd2489cabb1d
Neeraj Kumar <neeraj.paruthi087@gmail.com>
2023-07-12 16:41:53 +0530

    Update providers.tf

--- 3-Tier-Arch-using-Terraform-in-AWS/providers.tf
+++ 3-Tier-Arch-using-Terraform-in-AWS/providers.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = "1.5.2"
+    cloud {
+        organization = "DevOpsOrg"
 
+        workspaces {
+            tags = ["cloud:aws", "security"]
+        }
+    }
   required_providers {
     aws = {
       source = "hashicorp/aws"
